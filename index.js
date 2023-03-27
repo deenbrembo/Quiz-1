5c5e8fee18b09d810d5712fdf8bd0627eb2bd839
Hafizudin <hafizudnmuhammad83@gmail.com>
2023-03-27 16:51:50 +0800

    exercise 1

---
+++ index.js
@@ -0,0 +1,27 @@
+let data1 = true
+let data2 = "1.25"
+console.log(data1, typeof data1)
+console.log(data2, typeof data2)
+
+let data3 = data1.toString() + data2
+console.log(data3, typeof data3)
+
+
+//Array
+let data = ["one", 2, true]
+console.log( data[2], typeof data[2])
+
+
+//Object
+let user = {
+    name: "deenencem",
+    faculty: "FKEKK",
+    phone: {
+        Aku: "0105678302",
+        Emak:   "0104004015"},
+    email: "hafizudinmuhammad83@gmail.com"
+}
+console.log(user.name)
+console.log(user.faculty)
+console.log(user.phone.Aku)
+console.log(user.email)
